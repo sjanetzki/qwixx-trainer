Close #7: Fix calculation process of points and show intermediate points

game.py
@@ -1,11 +1,11 @@
 """This file creates an algorithm (Game) that leads the game through its course"""
-from typing import List
+from typing import List, Tuple
 
 from dice import Dice
 from board import Board, Row
 from ai_player import AiPlayer
 from human_player import HumanPlayer
-from player import CrossPossibility
+from player import CrossPossibility, Player
 from ui_pygame import PyGameUi
 import numpy as np
 import pickle
@@ -41,11 +41,9 @@ class Game:
             return True
         return False
 
-    def compute_ranking(self) -> List[List[tuple]]:
+    def compute_ranking(self) -> List[Tuple[Player, int]]:
         """computes a ranking of the players by points after the game is completed; function used by trainer"""
         ranking = []
-        if not self._is_completed():
-            return None
         for player in self.lst_player:
             points = player.get_points()
             ranking.append((player, points))
@@ -178,7 +176,7 @@ class Game:
             if len(turns) == 2:
                 is_turn_valid = self._make_valid_turn(player_index, turns[1], valid_turns, is_active_player, turns[0])
 
-    def play(self) -> None:
+    def play(self, prints_points=False) -> None:
         """manages the run of a game (Game Master) until the game is completed; directs when the players are prompted to
          do their turns; also used by the trainer"""
         game_in_progress = True
@@ -193,8 +191,10 @@ class Game:
                     else:
                         self._make_turns_for_ai_or_passive_human_player(lst_eyes, player_index, player,
                                                                         is_active_player)
+                if prints_points:
+                    print(self.compute_ranking())
+
                 if self._is_completed():
-                    # print(self.compute_ranking())
                     game_in_progress = False
 
                 # inform all players about new game situation AFTER they made their turns
@@ -218,8 +218,8 @@ if __name__ == "__main__":
     ui.show_board()
     ai_opponent = load_best_ai()
 
-    # game = Game([HumanPlayer("meep", 2, ui),
-                 # AiPlayer("meeep", 2, np.random.randn(18), np.random.randn(18), np.random.randn(18))])
+    # game = Game([HumanPlayer("meep", 1, ui),
+                 # AiPlayer("meeep", 1, np.random.randn(18), np.random.randn(18), np.random.randn(18))])
 
-    game = Game([HumanPlayer("meep", 2, ui), ai_opponent])
-    game.play()
+    game = Game([HumanPlayer("meep", 1, ui), ai_opponent])
+    game.play(True)

player.py
@@ -111,34 +111,22 @@ class Player(ABC):
                 situation[player_count * parameter_type + player_index] = situation_value
         return situation
 
-    @staticmethod
-    def _get_points_situation_(situation):
-        """calculates current points of all players"""
-        player_count = int(len(situation) / 9)
-        player_situation_sums = []
-        points_situation = np.zeros((player_count * 5,))  # we ignore row limits
-        for situation_index in range(player_count * 4):
-            colored_row_number = situation[situation_index]
-            points_situation[situation_index] = (colored_row_number ** 2 + colored_row_number) / 2
-        for situation_index in range(player_count * 4, player_count * 5):
-            penalty = situation[situation_index]
-            points_situation[situation_index] = penalty * (-5)
-        for player_index in range(player_count):
-            sum_points = 0
-            for parameter_type in range(5):
-                sum_points += points_situation[player_count * parameter_type + player_index]
-            player_situation_sums.append(sum_points)
-        return player_situation_sums
-
     def get_points(self):
-        """returns points of own situation"""
-        return self._get_points_situation_(self._get_situation())[-1]  # todo calculate points only for this player
+        """calculates current points of a player"""
+        situation = self._get_situation()
+        player_count = int(len(situation) / 9)
+        player_points = 0
+        player_index = player_count - 1
 
-    def display(self) -> None:
-        """prints the own board"""
-        print("number of opponents: {}".format(self.opponents))
-        print("{}´s BOARD: ".format(self.name))
-        self.board.show()
+        # gives points for number of crosses in a row
+        for row_start in range(0, player_count * 8, player_count * 2):  # will be called 4 times
+            colored_row_number = situation[row_start + player_index]
+            player_points += (colored_row_number ** 2 + colored_row_number) / 2
+
+        # calculates points for penalties
+        penalty = situation[player_count * 8 + player_index]
+        player_points += penalty * (-5)
+        return player_points
 
     def end(self, points) -> None:
         """prints the own points"""